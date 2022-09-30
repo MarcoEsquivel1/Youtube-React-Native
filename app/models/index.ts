532b3f2e8c3956289615fd4772926062b1d9f74e
Rene Sanabria <rsanabria@foodlogiq.com>
2022-09-30 14:28:53 -0600

    New Ignite 8.0.0-beta.8 app

---
+++ app/models/index.ts
@@ -0,0 +1,4 @@
+export * from "./RootStore"
+export * from "./helpers/get-root-store"
+export * from "./helpers/use-stores"
+export * from "./helpers/setup-root-store"
---
+++ test/mock-i18n.ts
@@ -0,0 +1,6 @@
+jest.mock("i18n-js", () => ({
+  currentLocale: () => "en",
+  t: (key: string, params: Record<string, string>) => {
+    return `${key} ${JSON.stringify(params)}`
+  },
+}))
